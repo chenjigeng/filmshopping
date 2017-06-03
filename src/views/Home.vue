c8d5855321634316496512481552e113e6fe09b5
陈纪庚 <178854407@qq.com>
2017-06-03 10:39:51 +0800

    修改了文件目录结构

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template lang='jade'>
+.home
+  h1 这是主页
+  lr-dialog
+</template>
+
+<script>
+import LRdialog from '@/components/LRdialog'
+export default {
+  name: 'Home',
+  data () {
+    return {
+      myTheme: 'light'
+    }
+  },
+  components: {
+    'lr-dialog': LRdialog
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+h1
+  color: red
+h2
+  color: blue
+</style>
+
